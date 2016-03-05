df7211f013891bfe9ad7ab568d29048598c4dc10
Chase Replogle <Chase Replogle>
2016-03-04 19:57:22 -0600

    Mobile Nav

--- js/min/navigation-min.js
+++ js/min/navigation-min.js
@@ -1,1 +1,1 @@
-jQuery(document).ready(function($){$(".mobile_menu").hide(),$(".mobile_toggle").click(function(){$(".mobile_menu").toggle()})});
+jQuery(document).ready(function($){$(".mobile_menu").hide(),$(window).resize(function(){$(window).width()<=850&&$("#sticky").attr("id","not_sticky")}),$(".mobile_toggle").click(function(){$(".mobile_menu").toggle()})});
--- js/navigation.js
+++ js/navigation.js
@@ -2,6 +2,13 @@ jQuery(document).ready(function( $ ) {
 
 	$(".mobile_menu").hide();
 
+	$(window).resize(function(){
+		if ($(window).width() <= 850){
+			$("#sticky").attr("id", "not_sticky");
+		}
+	});
+
+
 	$(".mobile_toggle").click(function(){
     	$(".mobile_menu").toggle();
 	});
