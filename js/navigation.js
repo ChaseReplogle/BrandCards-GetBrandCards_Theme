736128d87c4cdb306a9660101cc631d5c24a2121
Chase Replogle <Chase Replogle>
2016-03-05 22:42:26 -0600

    Fix Header

--- js/navigation.js
+++ js/navigation.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function( $ ) {
 	$(".mobile_menu").hide();
 
 	$(window).resize(function(){
-		if ($(window).width() <= 850){
+		if ($(window).width() <= 800){
 			$("#sticky").attr("id", "not_sticky");
 		}
 	});
