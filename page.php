cfa8e5a714d575c3be1515b620e09412a069c32e
Chase Replogle <Chase Replogle>
2016-03-08 15:30:37 -0600

    Page Template

--- page.php
+++ page.php
@@ -19,19 +19,8 @@ get_header(); ?>
 			<div class="single-learn-article">
 						<div class="single-learn-article_wrapper container row">
 							<div class="entry-content">
-								<?php
-									the_content( sprintf(
-										/* translators: %s: Name of current post. */
-										wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'get_brandcards' ), array( 'span' => array( 'class' => array() ) ) ),
-										the_title( '<span class="screen-reader-text">"', '"</span>', false )
-									) );
-
-									wp_link_pages( array(
-										'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'get_brandcards' ),
-										'after'  => '</div>',
-									) );
-								?>
-							</div><!-- .entry-content -->
+								<?php the_content(); ?>
+							</div>
 						</div>
 					</div>
 					</main>
