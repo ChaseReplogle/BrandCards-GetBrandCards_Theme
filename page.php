eb190c6a2347d0780c8f433b6509881a75ac43c8
Chase Replogle <Chase Replogle>
2016-03-08 15:28:22 -0600

    Page Template

--- page.php
+++ page.php
@@ -18,13 +18,20 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<div class="single-learn-article">
 						<div class="single-learn-article_wrapper container row">
-							<?php
-							while ( have_posts() ) : the_post();
+							<div class="entry-content">
+								<?php
+									the_content( sprintf(
+										/* translators: %s: Name of current post. */
+										wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'get_brandcards' ), array( 'span' => array( 'class' => array() ) ) ),
+										the_title( '<span class="screen-reader-text">"', '"</span>', false )
+									) );
 
-								get_template_part( 'template-parts/content', get_post_format() );
-
-							endwhile; // End of the loop.
-							?>
+									wp_link_pages( array(
+										'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'get_brandcards' ),
+										'after'  => '</div>',
+									) );
+								?>
+							</div><!-- .entry-content -->
 						</div>
 					</div>
 					</main>
