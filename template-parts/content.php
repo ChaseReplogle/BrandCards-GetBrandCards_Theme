75ec504b808e88a4e812ef3ba02cb97dd69b9033
Chase Replogle <Chase Replogle>
2016-03-08 14:50:57 -0600

    Learn Single

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,6 +11,9 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+		<div class="back_learn">
+			<a href="/learn">&#8592; Back</a>
+		</div>
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php get_brandcards_posted_on(); ?>
