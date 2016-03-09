f7c3535b1c2314838b3ba6a28dd2aa6e20a8087f
Chase Replogle <Chase Replogle>
2016-03-09 10:31:30 -0600

    Update Learn Section

--- archive.php
+++ archive.php
@@ -12,30 +12,34 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+			<div class="archive_header">
+				<div class="container row">
+					<h3>File Formats</h3>
+				</div>
+			</div>
+
+			<div class="posts">
+			<div class="back_learn container row">
+				<a href="/learn">&#8592; Back</a>
+			</div>
+			<div class="container row gutters">
+
+
 		<?php
 		if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
 			<?php
 			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			while ( have_posts() ) : the_post(); ?>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+				<div class="col span_8">
+					<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
+					<a href="<?php the_permalink(); ?>"><?php if ( has_post_thumbnail() ) { the_post_thumbnail(); }  ?></a>
+					<?php the_excerpt(); ?>
+				</div>
 
-			endwhile;
+			<?php endwhile;
 
-			the_posts_navigation();
 
 		else :
 
@@ -43,9 +47,11 @@ get_header(); ?>
 
 		endif; ?>
 
+			</div>
+			</div>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
