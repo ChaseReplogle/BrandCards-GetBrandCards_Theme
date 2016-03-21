f0a915a2a45c319946abca9d45df889ca8f5ea62
Chase Replogle <Chase Replogle>
2016-03-21 12:57:04 -0500

    Post Floats

--- page-learn.php
+++ page-learn.php
@@ -63,6 +63,7 @@ get_header(); ?>
 
 		<div class="posts">
 			<div class="container row gutters">
+					<div class="row">
 					<?php
 
 					$args = array(
@@ -70,18 +71,21 @@ get_header(); ?>
 					);
 
 					$the_query = new WP_Query( $args ); ?>
+					<?php $i = 0; ?>
 
 					<?php if ( $the_query->have_posts() ) : ?>
 
 
-						<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+						<?php while ( $the_query->have_posts() ) : $the_query->the_post();
+						if($i % 3 == 0) {echo '</div><div class="row">';} ?>
 
 							<div class="col span_8">
 								<a href="<?php the_permalink(); ?>"><?php if ( has_post_thumbnail() ) { the_post_thumbnail(); }  ?></a>
 								<a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
 								<?php the_excerpt(); ?>
 							</div>
-						<?php endwhile; ?>
+						<?php $i++;  endwhile; ?>
+						</div>
 
 						<?php wp_reset_postdata(); ?>
 
